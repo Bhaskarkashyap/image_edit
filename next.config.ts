9cf8f3742ba7115517a9d9519dd618f40151f9a8
Bhaskarkashyap <bhaskarkashyap02@gmail.com>
2025-04-16 01:22:50 +0530

    save changes

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     domains: ['pixabay.com'],
   },
+  experimental: {
+    allowedDevOrigins: ['*.csb.app'],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
